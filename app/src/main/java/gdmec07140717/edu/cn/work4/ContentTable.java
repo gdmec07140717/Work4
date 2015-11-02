22ca5c03ee7e287b5a90ceaaaf90d2e6107699d4
liyongji <1981560326@qq.com>
2015-11-02 21:29:02 +0800

    Initial commit

---
+++ app/src/main/java/gdmec07140717/edu/cn/work4/ContentTable.java
@@ -0,0 +1,33 @@
+package gdmec07140717.edu.cn.work4;
+
+/**
+ * Created by Administrator on 2015/10/30.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+
+public class ContentTable {
+    private  final static String TABLENAME="contactsTable";
+    private  MyDb db;
+    public ContentTable(Context context){
+        db =new MyDb(context);
+        if(!db.isTableExit(TABLENAME)){
+            String createtableSql = "create table if not exists"+TABLENAME+"(id_DB integer,"+"primary key AUTOINCREMENT,"+
+                    MyUser.getName() +"VARCHAR,"+
+                    MyUser.getMoblie()+"VARCHAR,"+
+                    MyUser.getQq()+"VARCHAR,"+
+                    MyUser.getDdanwei()+"VARCHAR,"+
+                    MyUser.getAddress()+"VARCHAR,";
+            db.CreatTabel(createtableSql);
+        }
+    }
+    public boolean addData(MyUser user){
+        ContentValues values = new ContentValues();
+        values.put(MyUser.getName(),user.getName());
+        values.put(MyUser.getMoblie(),user.getMoblie());
+        values.put(MyUser.getQq(),user.getQq());
+        values.put(MyUser.getDdanwei(),user.getDdanwei());
+        values.put(MyUser.getAddress(),user.getAddress());
+        return db.save(TABLENAME,values);
+    }
+}
